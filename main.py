3d58497a5afad7ebb7933593d70e27fb13822949
Paul SaundersPaul Saunders <psaunder@ualberta.ca>
2019-02-08 16:50:11 -0700

    Added a check to see if the player wants to play again, I had to add another while loop to ensure it worked

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ from game import Board
 b = Board()
 
 # only for testing purposes
-while True:
-    b.displayPlayer()
-    try:
-        col = input("choose column (1-7): ")
-        col = int(col)
-        assert 0 < col and col < 8
-    except:
-        print("please enter a number from 1 to 7")
-    else:
-        b.play(int(col) - 1)
-        b.getBoard()
-        if b.checkWin():
-            print('player %i won' % b.turn)
-            b.restart()
+endProgram = False
+endGame = False
+while not endProgram:
+    while not endGame:
+        b.displayPlayer()
+        try:
+            col = input("choose column (1-7): ")
+            col = int(col)
+            assert 0 < col and col < 8
+        except:
+            print("Please enter a number from 1 to 7")
         else:
-            print('player %i played' % b.turn)
-            b.changeTurn()
-        print('\n\n\n')
+            b.play(int(col) - 1)
+            b.getBoard()
+            if b.checkWin():
+                print('player %i won' % b.turn)
+                b.restart()
+            else:
+                print('player %i played' % b.turn)
+                b.changeTurn()
+            print('\n\n\n')
+
+    end = ''
+    while end.upper() not in ['Y', 'N']:
+        end = input("Would you like to play again (Y/N)? ")
+    if end.upper() == 'N':
+        endProgram = False
+
